fb8f6d09175af35b9f6d59e1e8c672a2475b92a1
Andrew <andrewkcutler@gmail.com>
2022-08-27 21:03:41 -0400

    logos

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,13 +1,107 @@
 import React, { Flex } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 import GradientBorder from '../gradient-border/GradientBorder';
+import jsLogo from '../../assets/images/js-logo.png';
+import angularLogo from '../../assets/images/angular-logo.jpg';
+import reactLogo from '../../assets/images/react-logo.png';
 import StackSection from '../stack-section/StackSection';
+import StackRow from '../stack-section/StackRow';
+
+const frontEndRows = [{}];
 
 const Skills = (): ReactElement => (
   <Flex flexDirection='column'>
-    <StackSection title='Front-end' />
-    <StackSection invert title='Back-end' />
-    <StackSection title='All the rest' />
+    <StackSection title='Front-end'>
+      <StackRow
+        imgSrc={jsLogo}
+        imgAlt='JavaScript'
+        subtitle='JavaScript'
+        textContent='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem enim
+            eligendi illo! Necessitatibus, fuga vitae. Nam similique quod maxime
+            iusto iure, pariatur suscipit vero explicabo esse aspernatur dicta
+            sunt illum.'
+      />
+      <StackRow
+        invert
+        imgSrc={reactLogo}
+        imgAlt='React'
+        subtitle='React'
+        textContent='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem enim
+            eligendi illo! Necessitatibus, fuga vitae. Nam similique quod maxime
+            iusto iure, pariatur suscipit vero explicabo esse aspernatur dicta
+            sunt illum.'
+      />
+      <StackRow
+        imgSrc={angularLogo}
+        imgAlt='Angular'
+        subtitle='Angular'
+        textContent='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem enim
+            eligendi illo! Necessitatibus, fuga vitae. Nam similique quod maxime
+            iusto iure, pariatur suscipit vero explicabo esse aspernatur dicta
+            sunt illum.'
+      />
+    </StackSection>
+    <StackSection invert title='Back-end'>
+      <StackRow
+        invert
+        imgSrc={jsLogo}
+        imgAlt='JavaScript'
+        subtitle='JavaScript'
+        textContent='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem enim
+            eligendi illo! Necessitatibus, fuga vitae. Nam similique quod maxime
+            iusto iure, pariatur suscipit vero explicabo esse aspernatur dicta
+            sunt illum.'
+      />
+      <StackRow
+        imgSrc={reactLogo}
+        imgAlt='React'
+        subtitle='React'
+        textContent='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem enim
+            eligendi illo! Necessitatibus, fuga vitae. Nam similique quod maxime
+            iusto iure, pariatur suscipit vero explicabo esse aspernatur dicta
+            sunt illum.'
+      />
+      <StackRow
+        invert
+        imgSrc={angularLogo}
+        imgAlt='Angular'
+        subtitle='Angular'
+        textContent='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem enim
+            eligendi illo! Necessitatibus, fuga vitae. Nam similique quod maxime
+            iusto iure, pariatur suscipit vero explicabo esse aspernatur dicta
+            sunt illum.'
+      />
+    </StackSection>
+    <StackSection title='All the rest'>
+      <StackRow
+        imgSrc={jsLogo}
+        imgAlt='JavaScript'
+        subtitle='JavaScript'
+        textContent='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem enim
+            eligendi illo! Necessitatibus, fuga vitae. Nam similique quod maxime
+            iusto iure, pariatur suscipit vero explicabo esse aspernatur dicta
+            sunt illum.'
+      />
+      <StackRow
+        invert
+        imgSrc={reactLogo}
+        imgAlt='React'
+        subtitle='React'
+        textContent='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem enim
+            eligendi illo! Necessitatibus, fuga vitae. Nam similique quod maxime
+            iusto iure, pariatur suscipit vero explicabo esse aspernatur dicta
+            sunt illum.'
+      />
+      <StackRow
+        imgSrc={angularLogo}
+        imgAlt='Angular'
+        subtitle='Angular'
+        textContent='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem enim
+            eligendi illo! Necessitatibus, fuga vitae. Nam similique quod maxime
+            iusto iure, pariatur suscipit vero explicabo esse aspernatur dicta
+            sunt illum.'
+      />
+    </StackSection>
   </Flex>
 );
 
